8c150202df15500729498549b9f61fbfe2733fde
John Bryant <john.r.bryant@gmail.com>
2023-03-12 10:05:28 +1300

    working on smooth_agetime

---
+++ R/check-functions.R
@@ -0,0 +1,95 @@
+
+
+## NO_TESTS
+#' Check a data frame of events or exposures
+#'
+#' We assume that the name of the data frame
+#' has the form "<measurevar>_df".
+#' 
+#' @param df The input data frame.
+#' @param measurevar The name of the measurement variable.
+#' @param timevar The name of the time variable.
+#' @param agevar The name of the age variable.
+#' @param byvar The names of additional classification variables.
+#' (May have length 0.)
+#'
+#' @returns TRUE, invisibly.
+#'
+#' @noRd
+check_input_df <- function(df,
+                           measurevar,
+                           timevar,
+                           agevar,
+                           byvar) {
+    nm_df <- paste0(measurevar, "_df")
+    nms_classif_vars <- c(timevar, agevar, byvar)
+    ## is data frame with no missing values,
+    ## at least 1 row, and at least 3 columns
+    checkmate::assert_data_frame(df,
+                                 min.rows = 1L,
+                                 min.cols = 3L,
+                                 .var.name = nm_df)
+    nms_df <- names(df)
+    ## has all required variables
+    for (vname in c(measurevar, nms_classif_vars)) {
+        if (!(vname %in% nms_df))
+        stop("'", nm_df, "' does not have a variable called \"",
+             vname, "\"")
+    }
+    ## check time var
+    timevar_val <- df[[timevar]]
+    val <- checkmate::check_integerish(timevar,
+                                       any.missing = FALSE)
+    if (!isTRUE(val))
+        stop("problem with variable '", timevar, "' in data frame '",
+             nm_df, "' : ", val)
+    ## check age var
+    agevar_val <- df[[agevar]]
+    if (is.numeric(agevar_val)) {
+        val <- checkmate::check_integerish(timevar,
+                                           any.missing = FALSE)
+        if (!isTRUE(val))
+            stop("problem with variable '", agevar, "' in data frame '",
+                 nm_df, "' : ", val)
+        complete_levels <- seq(from = min(val), to = max(val))
+        if (!setequal(val, complete_levels))
+            stop("variable '", agevar, "' in data frame '", nm_df,
+                 "' does not have complete set of levels")
+    }
+    else if (is.character(agevar_val) || is.factor(agevar_val)) {
+        if (anyNA(agevar_vl))
+            stop("variable '", agevar, "' in data frame '", nm_df,
+                 "' has NAs")
+    }
+    else
+        stop("variable '", agevar, "' in data frame '", nm_df,
+             "' has class \"", class(agevar_val), "\"")
+    ## check measure var
+    measurevar_val <- df[[measurevar]]
+    val <- checkmate::check_numeric(measurevar_val,
+                                    any.missing = FALSE,
+                                    lower = 0,
+                                    finite = TRUE)
+    if (!isTRUE(val))
+        stop("problem with variable '", measurevar, "' in data frame '",
+             nm_df, "' : ", val)
+    ## no duplicates of classification variables
+    classif_vars <- df[nms_classif_vars]
+    if (any(duplicated(classif_vars)))
+        stop("'", nm_df, "' has duplicate values for classifying variables [",
+             paste(nms_classif_vars, collapse = ", "), "]")
+    ## complete set of classification variables
+    n_row_expected <- prod(vapply(classif_vars, function(x) length(unique(x)), 1L))
+    n_row_obtained <- nrow(df)
+    if (nrow_obtained != nrow_expected)
+        stop("'", nm_df, "' missing combinations of classifying variables [",
+             paste(nms_classif_vars, collapse = ", "), "]")
+    ## return TRUE
+    invisible(TRUE)
+}
+                                    
+                              
+    
+
+
+                                 
--- R/smooth_agetime.R
+++ R/smooth_agetime.R
@@ -31,25 +31,67 @@ smooth_agetime <- function(nevent_df,
                            byvar = character(),
                            model_time = c("AR1", "LocalTrend"),
                            model_age = c("Spline", "RW2")) {
-  ## check inputs
-  checkmate::assert_string(timevar, min.chars = 1L)
-  checkmate::assert_string(agevar, min.chars = 1L)
-  if (identical(timevar, agevar))
-    stop("'timevar' and 'agevar' identical")
-  checkmate::assert_character(byvar,
-                              min.chars = 1L,
-                              any.missing = FALSE,
-                              unique = TRUE)
-  check_input_df(df = nevent_df,
-           measurevar = "nevent",
-                  timevar = timevar,
-                  agevar = agevar,
-                  byvar = byvar)
-  check_input_df(df = py_df,
-           measurevar = "py",
-                  timevar = timevar,
-                  agevar = agevar,
-                  byvar = byvar)
-
-  NULL
+    ## check variable names
+    checkmate::assert_string(timevar, min.chars = 1L)
+    checkmate::assert_string(agevar, min.chars = 1L)
+    checkmate::assert_character(byvar,
+                                min.chars = 1L,
+                                any.missing = FALSE,
+                                unique = TRUE)
+    if (identical(timevar, agevar))
+        stop("'timevar' and 'agevar' identical")
+    for (vname in c(timevar, agevar))
+        if (vname %in% byvar)
+            stop("'byvar' contains variable called \"", vname, "\"")
+    nms_classif_vars <- c(timevar, agevar, byvar)
+    ## check 'nevent_df' and 'py_df'
+    check_input_df(df = nevent_df,
+                   measurevar = "nevent",
+                   timevar = timevar,
+                   agevar = agevar,
+                   byvar = byvar)
+    check_input_df(df = py_df,
+                   measurevar = "py",
+                   timevar = timevar,
+                   agevar = agevar,
+                   byvar = byvar)
+    check_same_classif(nevent_df = nevent_df,
+                       py_df = py_df,
+                       nms_classif_vars = nms_classif_vars)
+    ## check 'model_time' and 'model_age'
+    model_time <- match.arg(model_time)
+    model_age <- match.arg(model_age)
+    ## construct datasets required for fitting
+    df <- merge(nevent_df[c(nms_classif_vars, "nevent")],
+                py_df[c(nms_classif_vars, "py")],
+                by = nms_classif_vars)
+    df[[timevar]] <- format_timevar(df[[timevar]])
+    df[[agevar]] <- format_agevar(df[[agevar]])
+    has_byvar <- length(byvar) > 0L
+    if (has_byvar)
+        l <- split(df, df[byvar])
+    else
+        l <- list(df)
+    make_nevent <- function(x)
+        stats::xtabs(nevent ~ age + time, data = x, addNA = TRUE)
+    make_py <- function(x)
+        stats::xtabs(py ~ age + time, data = x, addNA = TRUE)
+    nevent <- lapply(l, make_nevent)
+    py <- lapply(l, make_py)
+    ## do fitting
+    smoothed <- .mapply(smooth_agetime_inner,
+                        dots = list(nevent = nevent,
+                                    py = py),
+                        MoreArgs = list(model_time = model_time,
+                                        model_age = model_age))
+    ## assemble data frame with results
+    if (has_byvar) {
+        ans <- lapply(l, function(x) x[1L, byvar, drop = FALSE])
+        ans <- do.call(rbind, ans)
+    }
+    else
+        ans <- data.frame()
+    ans$smoothed <- smoothed
+    ## return
+    ans
 }
