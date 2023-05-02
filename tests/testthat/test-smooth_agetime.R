809444f28784544521a76fb300d3c6fa8797d341
John Bryant <john.r.bryant@gmail.com>
2023-05-02 17:32:19 +0700

    added test for 0/0

--- tests/testthat/test-smooth_agetime.R
+++ tests/testthat/test-smooth_agetime.R
@@ -13,6 +13,21 @@ test_that("'smooth_age' works with valid data - no 'by' variables", {
     expect_s3_class(ans, "BayesRates_results")
 })
 
+
+test_that("'smooth_age' works with valid data - exposure and count both zero", {
+    set.seed(0)
+    nevent_df <- tibble(age = 0:9,
+                        nevent = rpois(n = 10, lambda = 11:20))
+    py_df <- tibble(age = 0:9,
+                    py = 100)
+    nevent_df$nevent[3] <- 0
+    py_df$py[3] <- 0
+    ans <- smooth_age(nevent_df = nevent_df,
+                      py_df = py_df)
+    expect_s3_class(ans, "BayesRates_results")
+})
+
+
 ## 'smooth_agetime' -----------------------------------------------------------
 
 test_that("'smooth_agetime' works with valid data - no 'by' variables", {
